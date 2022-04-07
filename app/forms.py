0aeb22a8e095953713b1c3ada0a8e7537b61fac2
hamonhon <frozencookiez@gmail.com>
2022-04-07 18:26:34 +0800

    Major Overhaul

--- app/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import Email, Length, InputRequired
-
-class RegForm(FlaskForm):
-    email = StringField('Email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
-    password = PasswordField('Password', validators=[InputRequired(), Length(min=5, max=20)])
-    name = StringField('Name')
